d0499dff76133eb7f726ef48f3cf441d82a399f4
Brayan <brayan-lopes2020@outlook.com>
2023-08-18 17:13:21 -0300

    First function finally functioning as planned

---
+++ pptgame.js
@@ -0,0 +1,21 @@
+
+function pcho (){
+    let x = Math.floor(Math.random()*4);
+
+    if (x == 1){ 
+        return "Rock";
+    }
+    else if (x == 2){
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+
+
+
+
+
+
